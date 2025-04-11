6c89d4f543d02b90e73d5dfe7c969e2a8495bffc
LauraFojtikova <laurafojtikov@gmail.com>
2025-04-11 22:08:06 +0200

    feat: update filters

--- src/BL/Models/MovieFilterModel.cs
+++ src/BL/Models/MovieFilterModel.cs
@@ -13,6 +13,9 @@ namespace BL.Models
         public string? Genre {  get; set; }
         public int? ReleaseYear { get; set; }
         public bool? Favourite { get; set; }
+        public int? LengthMax { get; set; }
+        public MediaStatus? Status { get; set; }
+
         public string? SortBy { get; set; } = "title";
         public string? SortOrder { get; set; } = "asc";
     }
