6e4c58d6ef99a079fdd922bc01080d393a29b546
LauraFojtikova <laurafojtikov@gmail.com>
2025-05-17 14:51:05 +0200

    feat: add repository and mappers to DAL

---
+++ src/Vued/Vued.DAL/Mappers/IEntityMapper.cs
@@ -0,0 +1,9 @@
+using Vued.DAL.Entities;
+
+namespace Vued.DAL.Mappers;
+
+public interface IEntityMapper<in TEntity>
+    where TEntity : IEntity
+{
+    void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
+}
---
+++ src/Vued/Vued.DAL/Mappers/MovieEntityMapper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vued.DAL.Entities;
+
+namespace Vued.DAL.Mappers
+{
+    internal class MovieEntityMapper : IEntityMapper<Movie>
+    {
+        public void MapToExistingEntity(Movie existingEntity, Movie newEntity)
+        {
+            existingEntity.Id = newEntity.Id;
+            existingEntity.Name = newEntity.Name;
+            existingEntity.Description = newEntity.Description;
+            existingEntity.Status = newEntity.Status;
+            existingEntity.Duration = newEntity.Duration;
+            existingEntity.Director = newEntity.Director;
+            existingEntity.ReleaseDate = newEntity.ReleaseDate;
+            existingEntity.Rating = newEntity.Rating;
+            existingEntity.URL = newEntity.URL;
+            existingEntity.Favourite = newEntity.Favourite;
+            existingEntity.Watchlists = newEntity.Watchlists;
+            existingEntity.Genres = newEntity.Genres;
+        }
+    }
+}
