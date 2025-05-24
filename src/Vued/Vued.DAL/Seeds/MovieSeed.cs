226f2dbed7b87f0d3b5d948b6f5da4d8772df8cf
rm-a0 <michalrepcik9@gmail.com>
2025-05-24 16:18:55 +0200

    chore: create complex seeds for movies, series and genres

--- src/Vued/Vued.DAL/Seeds/MovieSeed.cs
+++ src/Vued/Vued.DAL/Seeds/MovieSeed.cs
@@ -1,5 +1,6 @@
 using Vued.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Vued.DAL.Seeds;
 
@@ -8,8 +9,146 @@ public static class MovieSeed
     public static void Seed(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Movie>().HasData(
-            new Movie { Id = 1, Name = "Inception", Length = 148, Status = MediaStatus.Completed, ReleaseDate = 2010, Rating = "PG-13" },
-            new Movie { Id = 2, Name = "The Matrix", Length = 136, Status = MediaStatus.Completed, ReleaseDate = 1999, Rating = "R" }
+            new Movie
+            {
+                Id = 1,
+                Name = "The Matrix",
+                Length = 136,
+                Status = MediaStatus.Completed,
+                Description = "A computer hacker learns about the true nature of reality and joins a group of rebels to fight a war against powerful controllers.",
+                Director = "Lana Wachowski, Lilly Wachowski",
+                ReleaseDate = 1999,
+                Rating = "8/10",
+                URL = "https://www.imdb.com/title/tt0133093/",
+                Favourite = true,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 1 }, // Action
+                    new Genre { Id = 5 }, // Science Fiction
+                    new Genre { Id = 44 }  // Cyberpunk
+                }
+            },
+            new Movie
+            {
+                Id = 2,
+                Name = "Forrest Gump",
+                Length = 142,
+                Status = MediaStatus.Completed,
+                Description = "The life story of a man with a low IQ who achieves extraordinary feats through his kindness and determination.",
+                Director = "Robert Zemeckis",
+                ReleaseDate = 1994,
+                Rating = "6/10", // Jane is a bitch (pretend you did not see this)
+                URL = "https://www.imdb.com/title/tt0109830/",
+                Favourite = true,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 3 }, // Drama
+                    new Genre { Id = 8 }, // Romance
+                    new Genre { Id = 28 }  // Coming-of-Age
+                }
+            },
+            new Movie
+            {
+                Id = 3,
+                Name = "The Shining",
+                Length = 144,
+                Status = MediaStatus.Completed,
+                Description = "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence.",
+                Director = "Stanley Kubrick",
+                ReleaseDate = 1980,
+                Rating = "7/10",
+                URL = "https://www.imdb.com/title/tt0081505/",
+                Favourite = false,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 4 }, // Horror
+                    new Genre { Id = 25 }, // Psychological
+                    new Genre { Id = 42 }  // Gothic
+                }
+            },
+            new Movie
+            {
+                Id = 4,
+                Name = "Inception",
+                Length = 148,
+                Status = MediaStatus.Completed,
+                Description = "A skilled thief with the ability to enter dreams must pull off an impossible heist: planting an idea in someone's mind.",
+                Director = "Christopher Nolan",
+                ReleaseDate = 2010,
+                Rating = "9/10",
+                URL = "https://www.imdb.com/title/tt1375666/",
+                Favourite = true,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 5 }, // Science Fiction
+                    new Genre { Id = 9 }, // Thriller
+                    new Genre { Id = 36 }  // Heist
+                }
+            },
+            new Movie
+            {
+                Id = 5,
+                Name = "The Godfather",
+                Length = 175,
+                Status = MediaStatus.Completed,
+                Description = "The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
+                Director = "Francis Ford Coppola",
+                ReleaseDate = 1972,
+                Rating = "8/10",
+                URL = "https://www.imdb.com/title/tt0068646/",
+                Favourite = true,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 11 }, // Crime
+                    new Genre { Id = 3 },  // Drama
+                    new Genre { Id = 29 }  // Epic
+                }
+            },
+            new Movie
+            {
+                Id = 6,
+                Name = "Spirited Away",
+                Length = 125,
+                Status = MediaStatus.Completed,
+                Description = "A young girl becomes trapped in a strange spirit world and must find a way to free herself and her parents.",
+                Director = "Hayao Miyazaki",
+                ReleaseDate = 2001,
+                Rating = "8/10",
+                URL = "https://www.imdb.com/title/tt0245429/",
+                Favourite = false,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 18 }, // Animation
+                    new Genre { Id = 6 },  // Fantasy
+                    new Genre { Id = 28 }  // Coming-of-Age
+                }
+            },
+            new Movie
+            {
+                Id = 7,
+                Name = "Mad Max: Fury Road",
+                Length = 120,
+                Status = MediaStatus.Completed,
+                Description = "In a post-apocalyptic wasteland, a drifter and a rebel leader fight to survive against a tyrannical ruler.",
+                Director = "George Miller",
+                ReleaseDate = 2015,
+                Rating = "7/10",
+                URL = "https://www.imdb.com/title/tt1392190/",
+                Favourite = true,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 1 },  // Action
+                    new Genre { Id = 26 }, // Post-Apocalyptic
+                    new Genre { Id = 7 }   // Adventure
+                }
+            }
         );
     }
 }
--- src/Vued/Vued.DAL/Seeds/SeriesSeed.cs
+++ src/Vued/Vued.DAL/Seeds/SeriesSeed.cs
@@ -1,5 +1,6 @@
 using Vued.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Vued.DAL.Seeds;
 
@@ -8,8 +9,153 @@ public static class SeriesSeed
     public static void Seed(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Series>().HasData(
-            new Series { Id = 3, Name = "Breaking Bad", NumberOfEpisodes = 62, Status = MediaStatus.Completed, ReleaseDate = 2008, Rating = "TV-MA" },
-            new Series { Id = 4, Name = "Stranger Things", NumberOfEpisodes = 34, Status = MediaStatus.Watching, ReleaseDate = 2016, Rating = "TV-14" }
+            new Series
+            {
+                Id = 1,
+                Name = "Breaking Bad",
+                NumberOfEpisodes = 62,
+                Status = MediaStatus.Completed,
+                Description = "A chemistry teacher turned drug-lord teams up with a former student to build a methamphetamine empire.",
+                Director = "Vince Gilligan",
+                ReleaseDate = 2008,
+                Rating = "10/10",
+                URL = "https://www.imdb.com/title/tt0903747/",
+                Favourite = true,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 3 },  // Drama
+                    new Genre { Id = 11 }, // Crime
+                    new Genre { Id = 9 }   // Thriller
+                }
+            },
+            new Series
+            {
+                Id = 2,
+                Name = "Stranger Things",
+                NumberOfEpisodes = 34,
+                Status = MediaStatus.Watching,
+                Description = "A group of friends in the 1980s uncover supernatural mysteries and government conspiracies in their small town.",
+                Director = "The Duffer Brothers",
+                ReleaseDate = 2016,
+                Rating = "8/10",
+                URL = "https://www.imdb.com/title/tt4574334/",
+                Favourite = true,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 5 },  // Science Fiction
+                    new Genre { Id = 4 },  // Horror
+                    new Genre { Id = 28 }  // Coming-of-Age
+                }
+            },
+            new Series
+            {
+                Id = 3,
+                Name = "The Office (US)",
+                NumberOfEpisodes = 201,
+                Status = MediaStatus.Completed,
+                Description = "A mockumentary about the daily lives of employees at the Dunder Mifflin paper company.",
+                Director = "Greg Daniels",
+                ReleaseDate = 2005,
+                Rating = "9/10",
+                URL = "https://www.imdb.com/title/tt0386676/",
+                Favourite = true,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 2 },  // Comedy
+                    new Genre { Id = 46 }, // Romantic Comedy
+                    new Genre { Id = 49 }  // Mockumentary
+                }
+            },
+            new Series
+            {
+                Id = 4,
+                Name = "Game of Thrones",
+                NumberOfEpisodes = 73,
+                Status = MediaStatus.Completed,
+                Description = "Noble families fight for control of the Iron Throne in a fantasy world filled with dragons and political intrigue.",
+                Director = "David Benioff, D.B. Weiss",
+                ReleaseDate = 2011,
+                Rating = "9/10",
+                URL = "https://www.imdb.com/title/tt0944947/",
+                Favourite = true,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 6 },  // Fantasy
+                    new Genre { Id = 3 },  // Drama
+                    new Genre { Id = 29 }  // Epic
+                }
+            },
+            new Series
+            {
+                Id = 5,
+                Name = "The Mandalorian",
+                NumberOfEpisodes = 24,
+                Status = MediaStatus.Watching,
+                Description = "A lone bounty hunter navigates the outer reaches of the galaxy, protecting a mysterious baby Yoda.",
+                Director = "Jon Favreau",
+                ReleaseDate = 2019,
+                Rating = "6/10",
+                URL = "https://www.imdb.com/title/tt8111088/",
+                Favourite = false,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 1 },  // Action
+                    new Genre { Id = 5 },  // Science Fiction
+                    new Genre { Id = 7 }   // Adventure
+                }
+            },
+            new Series
+            {
+                Id = 6,
+                Name = "Black Mirror",
+                NumberOfEpisodes = 27,
+                Status = MediaStatus.Watching,
+                Description = "An anthology series exploring the dark side of technology and human nature in dystopian futures.",
+                Director = "Charlie Brooker",
+                ReleaseDate = 2011,
+                Rating = "8/10",
+                URL = "https://www.imdb.com/title/tt2085059/",
+                Favourite = false,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 5 },  // Science Fiction
+                    new Genre { Id = 27 }, // Dystopian
+                    new Genre { Id = 40 }  // Anthology
+                }
+            },
+            new Series
+            {
+                Id = 7,
+                Name = "The Crown",
+                NumberOfEpisodes = 60,
+                Status = MediaStatus.PlanToWatch,
+                Description = "A biographical drama chronicling the reign of Queen Elizabeth II and major historical events.",
+                Director = "Peter Morgan",
+                ReleaseDate = 2016,
+                Rating = "-",
+                URL = "https://www.imdb.com/title/tt4786824/",
+                Favourite = false,
+                Duration = 0,
+                Watchlists = new List<Watchlist>(),
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 16 }, // Biography
+                    new Genre { Id = 13 }, // Historical
+                    new Genre { Id = 3 }   // Drama
+                }
+            }
         );
     }
 }
